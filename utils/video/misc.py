3f0f2b7b06cf6bc073948335b3d734e83376ccc3
Dmitry <dimasharonov95@yandex.ru>
2024-09-25 01:54:47 +0300

    Улучшены проверки возможности работы с nvenc

--- utils/video/misc.py
+++ utils/video/misc.py
@@ -1,6 +1,8 @@
 # https://developer.nvidia.com/video-encode-and-decode-gpu-support-matrix-new
 NVENC_MAX_CONCURRENT_SESSIONS = 8
 
+NVENC_CODECS = ("hevc_nvenc", "h264_nvenc", "av1_nvenc")  # todo: calculate based on gpu
+
 
 def ensure_nvenc_correct(use_nvenc: bool, force_video_codec: str | None, threads: int) -> bool | None:
     """
@@ -11,14 +13,17 @@ def ensure_nvenc_correct(use_nvenc: bool, force_video_codec: str | None, threads
     :param threads: число параллельных сессий
     """
     if not use_nvenc:
+
+        if force_video_codec in NVENC_CODECS:
+            raise ValueError(f"You must enable nvenc to use {force_video_codec}")
+
         return
 
     if force_video_codec is None:
         raise ValueError("You must specify video codec to use nvenc acceleration in ffmpeg")
 
-    allowed_codecs = ("hevc_nvenc", "h264_nvenc", "av1_nvenc")  # todo: calculate based on gpu
-    if force_video_codec not in allowed_codecs:
-        raise ValueError(f"Video codec must be in {allowed_codecs} if you use nvenc, got {force_video_codec}")
+    if force_video_codec not in NVENC_CODECS:
+        raise ValueError(f"Video codec must be in {NVENC_CODECS} if you use nvenc, got {force_video_codec}")
 
     if threads > NVENC_MAX_CONCURRENT_SESSIONS:
         raise ValueError(f"nvenc supports maximum {NVENC_MAX_CONCURRENT_SESSIONS} cuncurrent sessions")
